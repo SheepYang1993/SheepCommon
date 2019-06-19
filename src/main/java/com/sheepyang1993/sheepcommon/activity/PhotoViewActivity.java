d852651f146bdfc644dd72064ba6c6fcde10e6f9
SheepYang1993 <332594623@qq.com>
2019-06-19 09:44:06 +0800

    v100 新增用户头像查看

--- src/main/java/com/sheepyang1993/sheepcommon/activity/PhotoViewActivity.java
+++ src/main/java/com/sheepyang1993/sheepcommon/activity/PhotoViewActivity.java
@@ -3,9 +3,13 @@ package com.sheepyang1993.sheepcommon.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 
+import com.bumptech.glide.Glide;
 import com.github.chrisbanes.photoview.PhotoView;
 import com.sheepyang1993.sheepcommon.R;
+
+import static android.graphics.drawable.Icon.TYPE_BITMAP;
 
 /**
  * @author SheepYang
@@ -13,8 +17,14 @@ import com.sheepyang1993.sheepcommon.R;
  * @date 2019/6/8
  */
 public class PhotoViewActivity extends BaseActivity {
+    public static final int TYPE_BITMAP = 1;
+    public static final int TYPE_URL = 2;
+    private static final String EXTRA_TYPE = "EXTRA_TYPE";
+    private static final String EXTRA_URL = "EXTRA_URL";
     private static final String EXTRA_BITMAP = "EXTRA_BITMAP";
     private PhotoView photoView;
+    private int mType;
+    private String mUrl;
     private Bitmap mBitmap;
 
     @Override
@@ -29,13 +39,34 @@ public class PhotoViewActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        mBitmap = getIntent().getParcelableExtra(EXTRA_BITMAP);
-        photoView.setImageBitmap(mBitmap);
+        mType = getIntent().getIntExtra(EXTRA_TYPE, 0);
+        switch (mType) {
+            case TYPE_BITMAP:
+                mBitmap = getIntent().getParcelableExtra(EXTRA_BITMAP);
+                photoView.setImageBitmap(mBitmap);
+                break;
+            case TYPE_URL:
+                mUrl = getIntent().getStringExtra(EXTRA_URL);
+                Glide.with(photoView)
+                        .load(mUrl)
+                        .into(photoView);
+                break;
+            default:
+                break;
+        }
     }
 
     public static void startSelf(Context context, Bitmap bitmap) {
         Intent intent = new Intent(context, PhotoViewActivity.class);
+        intent.putExtra(EXTRA_TYPE, TYPE_BITMAP);
         intent.putExtra(EXTRA_BITMAP, bitmap);
+        context.startActivity(intent);
+    }
+
+    public static void startSelf(Context context, String url) {
+        Intent intent = new Intent(context, PhotoViewActivity.class);
+        intent.putExtra(EXTRA_TYPE, TYPE_URL);
+        intent.putExtra(EXTRA_URL, url);
         context.startActivity(intent);
     }
 }
