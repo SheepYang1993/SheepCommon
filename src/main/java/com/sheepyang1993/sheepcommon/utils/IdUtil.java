59fad1da9dd6cf2a06074d699959bbe9c45af338
SheepYang1993 <332594623@qq.com>
2019-07-23 18:24:33 +0800

    v100 抽取方法

--- src/main/java/com/sheepyang1993/sheepcommon/utils/IdUtil.java
+++ src/main/java/com/sheepyang1993/sheepcommon/utils/IdUtil.java
@@ -1,5 +1,13 @@
 package com.sheepyang1993.sheepcommon.utils;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
+import com.blankj.utilcode.util.PhoneUtils;
+import com.blankj.utilcode.util.Utils;
+
 /**
  * @author SheepYang
  * @email 332594623@qq.com
@@ -7,4 +15,21 @@ package com.sheepyang1993.sheepcommon.utils;
  * @describe Id相关工具类
  */
 public class IdUtil {
+    public static String getImei() {
+        if (ActivityCompat.checkSelfPermission(Utils.getApp(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "";
+        }
+        return PhoneUtils.getIMEI();
+    }
+
+    public static String getUuid() {
+        return UUIDUtil.uuid();
+    }
 }
