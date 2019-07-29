dca1c5ff2091ba935c99da5129f8bd4c7c0ddd81
SheepYang1993 <332594623@qq.com>
2019-07-29 16:48:16 +0800

    v100 调整debug工具

--- src/main/java/com/sheepyang1993/sheepcommon/utils/DebugUtil.java
+++ src/main/java/com/sheepyang1993/sheepcommon/utils/DebugUtil.java
@@ -1,5 +1,7 @@
 package com.sheepyang1993.sheepcommon.utils;
 
+import com.blankj.utilcode.util.SPUtils;
+
 /**
  * @author SheepYang
  * @email 332594623@qq.com
@@ -7,12 +9,18 @@ package com.sheepyang1993.sheepcommon.utils;
  * @describe Debug工具类
  */
 public class DebugUtil {
+    private static final String SP_DEBUG_ENV = "SP_DEBUG_ENV";
+
     /**
      * 是否测试环境
      *
      * @return
      */
     public static boolean isDebugEnv() {
-        return true;
+        return SPUtils.getInstance().getBoolean(SP_DEBUG_ENV, false);
+    }
+
+    public static void setDebugEnv(boolean isDebug) {
+        SPUtils.getInstance().put(SP_DEBUG_ENV, isDebug);
     }
 }
