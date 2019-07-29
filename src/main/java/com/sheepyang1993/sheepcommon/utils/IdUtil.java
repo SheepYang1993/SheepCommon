6974428081e54ca7dbab781309011add63771aed
SheepYang <332594623@qq.com>
2019-07-29 08:19:50 +0800

    。。

--- src/main/java/com/sheepyang1993/sheepcommon/utils/IdUtil.java
+++ src/main/java/com/sheepyang1993/sheepcommon/utils/IdUtil.java
@@ -1,7 +1,6 @@
 package com.sheepyang1993.sheepcommon.utils;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 
