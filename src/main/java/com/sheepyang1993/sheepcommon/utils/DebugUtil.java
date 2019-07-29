0057996be52fbd50fcbd01febb15525eadaada94
SheepYang1993 <332594623@qq.com>
2019-07-29 16:14:45 +0800

    v100 新增debug工具

---
+++ src/main/java/com/sheepyang1993/sheepcommon/utils/DebugUtil.java
@@ -0,0 +1,18 @@
+package com.sheepyang1993.sheepcommon.utils;
+
+/**
+ * @author SheepYang
+ * @email 332594623@qq.com
+ * @date 2019/7/29 16:13
+ * @describe Debug工具类
+ */
+public class DebugUtil {
+    /**
+     * 是否测试环境
+     *
+     * @return
+     */
+    public static boolean isDebugEnv() {
+        return true;
+    }
+}
