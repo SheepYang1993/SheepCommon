3e23cf2fc24ba947315ed8fe7931e3f92ffc1a32
SheepYang1993 <332594623@qq.com>
2019-07-30 16:33:00 +0800

    v100 新增过滤操作

--- src/main/java/com/sheepyang1993/sheepcommon/adapter/LogAdapter.java
+++ src/main/java/com/sheepyang1993/sheepcommon/adapter/LogAdapter.java
@@ -1,5 +1,9 @@
 package com.sheepyang1993.sheepcommon.adapter;
 
+import android.text.TextUtils;
+import android.view.ViewGroup;
+
+import com.blankj.utilcode.util.RegexUtils;
 import com.blankj.utilcode.util.TimeUtils;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -16,6 +20,7 @@ import java.util.Locale;
  * @date 2019/7/27
  */
 public class LogAdapter extends BaseQuickAdapter<LogModel, BaseViewHolder> {
+    private String mFilterStr = "";
     private DateFormat mFormat = new SimpleDateFormat("HH:mm:ss SSS", Locale.getDefault());
 
     public LogAdapter() {
@@ -24,9 +29,31 @@ public class LogAdapter extends BaseQuickAdapter<LogModel, BaseViewHolder> {
 
     @Override
     protected void convert(BaseViewHolder helper, LogModel item) {
+        String tag = item.getTag();
         helper.setText(R.id.tvTime, TimeUtils.millis2String(item.getCreateTime(), mFormat));
-        helper.setText(R.id.tvTag, item.getTag());
+        helper.setText(R.id.tvTag, tag);
         helper.setText(R.id.tvLog, item.getMsg());
         helper.addOnLongClickListener(R.id.tvLog);
+        if (!TextUtils.isEmpty(mFilterStr)) {
+            boolean show = mFilterStr.contains(tag);
+            hideItem(helper, show);
+        } else {
+            hideItem(helper, true);
+        }
+    }
+
+    private void hideItem(BaseViewHolder helper, boolean show) {
+        ViewGroup.LayoutParams lp = helper.itemView.getLayoutParams();
+        if (show) {
+            lp.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        } else {
+            lp.height = 0;
+        }
+        helper.itemView.setLayoutParams(lp);
+    }
+
+    public void setFilterStr(String filterStr) {
+        mFilterStr = filterStr;
+        notifyDataSetChanged();
     }
 }
